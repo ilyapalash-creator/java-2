fe4d8c0fc0b2183a78046a9da4e7abd0bb2f5852
Ilya Palash <rudut.ilya@ya.ru>
2026-03-20 13:34:33 +0300

    Second commit

--- src/MAIN.java
+++ src/MAIN.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class MAIN {
+
+    public static void main(String[] args){
+
+        int cost_of_ticket = 11_225;
+        int bonus_miles = cost_of_ticket / 20;
+        System.out.println( bonus_miles );
+
+
+    }
 }
