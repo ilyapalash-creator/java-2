b1ab963aaab23d1a463f9a93fe3c9e6450c635aa
Ilya Palash <rudut.ilya@ya.ru>
2026-03-23 19:19:45 +0300

    Final commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        int costOfTicket = 11_225;
+        int bonusMiles = costOfTicket / 20;
+        System.out.println(bonusMiles);
+
+
+    }
+}
